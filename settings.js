2a60043c7550c59a83257ccdf11f3941a1f54ca5
Zain <zain6787865@gmail.com>
2026-01-19 22:24:54 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𓆩 HUNTER-MD 𓆪',
+  author: '‎',
+  botName: "𓆩 HUNTER-MD 𓆪",
+  botOwner: 'SILVER', // Your name
+  ownerNumber: '923073449859', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.6",
+  updateZipUrl: "https://github.com/zain-editor/-HUNTER-MD-",
+};
+
+module.exports = settings;
